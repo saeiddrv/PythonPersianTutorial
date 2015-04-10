1cafbdf2133b366ccbb245f22c4a757285f82d71
Saeid Darvishi <saeid.dq@gmail.com>
2015-04-10 21:52:18 +0430

    Original files

--- conf.py
+++ conf.py
@@ -16,6 +16,8 @@ import sys
 import os
 import shlex
 
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -116,7 +118,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
