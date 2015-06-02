829456518f77341edf8308f08455cfda2859d488
Saeid Darvishi <saeid.dq@gmail.com>
2015-06-02 12:15:45 +0430

    upgrade to 0.9.5 : Minoo theme

--- _templates/sphinx_minoo_theme/__init__.py
+++ _templates/sphinx_minoo_theme/__init__.py
@@ -9,7 +9,7 @@ https://github.com/saeiddrv/SphinxMinooTheme.
 class MinooVersion():
     major = "0"
     minor = "9"
-    micro = "3"
+    micro = "5"
     level = "Beta"
     release = r"2015/05/21"
     def info(self):
