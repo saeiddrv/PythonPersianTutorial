7b18e47f7802ff33ccd0d85631ef9ab0af4920bf
Saeid Darvish <saeid.dq@gmail.com>
2023-03-29 22:56:23 +0330

    upgarde sphinx

---
+++ source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PythonPersianTutorial'
+copyright = '2015, Saeid Darvish'
+author = 'Saeid Darvish'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinxcontrib.jquery"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fa'
+
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_minoo_theme'
+html_theme_path = ["_templates"]
+html_static_path = ['_static']
