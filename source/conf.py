4ce9909e27a39a80a4f0f4ea003d4c850f1bce0b
Saeid Darvish <saeid.dq@gmail.com>
2023-03-31 14:17:25 +0330

    changed title

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PythonPersianTutorial'
+project = 'پایتون به پارسی'
 copyright = '2015, Saeid Darvish'
 author = 'Saeid Darvish'
 
